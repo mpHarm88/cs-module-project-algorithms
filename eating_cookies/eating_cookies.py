752366cfabdea7b2ddffa87e9b6adb5411659522
mpHarm88 <mpHarm88@users.noreply.github.com>
2020-08-12 20:26:44 -0700

    add first pass solutions

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,3 +1,4 @@
+import itertools as it
 '''
 Input: an integer
 Returns: an integer
@@ -5,15 +6,13 @@ Returns: an integer
 def eating_cookies(n):
     # Your code here
 
-    ls = [1,2,3]
+    if n==0:
+        return 1
+    elif n < 0:
+        return 0  
+    else:
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
-    count = 0
-
-    for x in ls:
-        
-        
-
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
